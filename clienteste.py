f07fc6e58591dc781283ff1f3c172aa63422287b
@NatanMoreira1 <natan-moreira1@hotmail.com>
2019-10-05 20:40:08 -0300

    Broadcast complete

--- clienteste.py
+++ clienteste.py
@@ -8,21 +8,24 @@ from aquario import aquario
 host = ''
 port = 5680
 
-meia = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-meia.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 2)
-meia.bind((host,port))
+def config_socket(host,port):
+  meia = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+  meia.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+  meia.bind((host,port))
+  return meia
 
 
+cliente = config_socket(host,port)
 aquario1 = aquario('Aquario1',comida = 10)
 
-while True:
-  data,address = meia.recvfrom(1024)
-  print("Address: {}\n".format(address))
+while True:  
+  data,address = cliente.recvfrom(1024)
   data = p.loads(data)
-
   if data == 'aquario1':
+    print(f"Address:{address}")
     msg = p.dumps(aquario1.get_nome())
-    meia.sendto(msg,address)
+    cliente.sendto(msg,address)
+    print("Mensagem enviada!")
 
-  print("Mensagem enviada!")
+  
   
--- servidorteste.py
+++ servidorteste.py
@@ -2,17 +2,18 @@ import socket
 import time
 import pickle as p 
 
+def config_serve(host="",port=5000):
+  server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+  server.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+  server.settimeout(0.2)
+  server.bind((host, port))
+  return server
 
-server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
-server.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 2)
 
-
-server.settimeout(0.2)
-server.bind(("", 44444))
-
+servidor = config_serve()
 msg = p.dumps("aquario1")
 
-server.sendto(msg, ('<broadcast>', 5680))
+servidor.sendto(msg, ('<broadcast>', 5680))
 time.sleep(1)
-data,address = server.recvfrom(1024)
+data,address = servidor.recvfrom(1024)
 print(p.loads(data))
