2f36cd86eca3dad15b694c86beeea5b4e9fcbb16
RamiroCampos <43554825+RamiroCampos@users.noreply.github.com>
2019-10-05 18:52:48 -0300

    Create portao.py

---
+++ Classes/portao.py
@@ -0,0 +1,39 @@
+#Classe Portao
+# Nome_Portao :
+# Aberto: Aberto / Fechado
+# Ativar: Abre / Fecha o Portao
+# Lembrar: Se o Portao ficar mais de dois minutos aberto, manda um aviso
+
+import random
+import time 
+
+class aquario:
+  def __init__(self,nome, aberto = True, lembrar = False):
+    self.nome = nome
+    self.cond = cond
+    self.lembrar = lembrar
+  
+  def get_nome(self):
+    return self.nome 
+
+  def get_estado_portao(self):
+    if self.aberto == True:
+      return "Aberto" 
+    else:
+      return "Fechado"
+  
+  def set_ativar(self):
+    if self.aberto == True:
+      #função para fechar o portao
+      self.aberto = False:
+      return "O portão foi aberto" 
+    else:
+      self.aberto = True:
+      return "O portão foi fechado"
+
+  def lembrar(self):
+    if self.aberto == True: 
+      time_start = time.time();
+      while(self.aberto) :
+        if((time.time() - time_start) > 120):
+          return("O Portao está aberto!")
