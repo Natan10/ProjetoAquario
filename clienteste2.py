408d587dcbab323ea914f5896feeaa5c8fd09108
@NatanMoreira1 <natan-moreira1@hotmail.com>
2019-10-13 11:03:14 -0300

    portao e mensagens ok

--- clienteste2.py
+++ clienteste2.py
@@ -5,7 +5,7 @@ import pickle as p
 sys.path.append('./Classes')
 from aquario import Aquario
 
-host = 'localhost'
+host = ''
 port = 5680
 
 def config_socket(host,port):
@@ -20,7 +20,7 @@ aquario2 = Aquario('Aquario2',comida = 15)
 
 cliente.sendto(p.dumps(['1','aq2']),('',5000))
 
-funcoes = ['get_nome','get_estado_luz','set_estado_luz','get_qtd_comida','set_estado_comer','set_estado_addcomida','get_estado_filtro']
+funcoes = ['1:get_nome','2:get_estado_luz','3:get_qtd_comida','4:get_estado_filtro','5:get_estado_aquario','6:set_estado_luz','7:set_estado_comer','8:set_estado_addcomida']
 func = ['nome','comida','luz']
 
 print("Iniciando Aquario2...")
@@ -31,21 +31,53 @@ while True:
 
     if data[1] == 'aq2' and data[2] == 'list':
       print(address) 
-      msg = ['2',func]
+      msg = ['2',funcoes]
       cliente.sendto(p.dumps(msg),('',5000))
 
-    elif data[0] == '1' and data[1] == 'nd':
+    elif data[0] == '1' and data[1] == 'ping':
       print(address)
       msg = ['1','aq2']
       cliente.sendto(p.dumps(msg),('',5000))
 
-    elif data[1] == 'aq2' and data[2] in funcoes:
-      aux = funcoes[funcoes.index(data[2])]
-      if aux ==  
+    elif data[1] == 'aq2':
       print(address)
-      msg = ['2',getattr(aquario2,data[2])]
-      cliente.sendto(p.dumps(msg),('localhost',5000)) 
+      if data[2] == '1':
+        msg = ['2',aquario2.nome]
+        cliente.sendto(p.dumps(msg),('',5000)) 
+      
+      elif data[2] == '2':
+        msg = ['2',aquario2.get_estado_luz()]
+        cliente.sendto(p.dumps(msg),('',5000))
+      
+      elif data[2] == '3':
+        msg = ['2',aquario2.get_qtd_comida()]
+        cliente.sendto(p.dumps(msg),('',5000))
 
+      elif data[2] == '4':
+        msg = ['2',aquario2.get_estado_filtro()]
+        cliente.sendto(p.dumps(msg),('',5000))
+      
+      elif data[2] == '5':
+        msg = ['2',aquario2.get_estado_aquario()]
+        cliente.sendto(p.dumps(msg),('',5000))
+
+      elif data[2] == '6' and not(not data[3]):
+        aquario2.set_estado_luz(int(data[3]))
+        msg = ['2',aquario2.luz]
+        cliente.sendto(p.dumps(msg),('',5000))
+
+      elif data[2] == '7' and not(not data[3]):
+        aux = aquario2.set_estado_comer(int(data[3]))
+        if not aux:
+          msg = ['2',aquario2.get_qtd_comida()]
+        else:
+          msg = ['2',aux]
+        cliente.sendto(p.dumps(msg),('',5000))
+      
+      elif data[2] == '8' and not(not data[3]):
+        aquario2.set_estado_addcomida(int(data[3]))
+        msg = ['2',aquario2.comida]
+        cliente.sendto(p.dumps(msg),('',5000))
   except OSError as msg:
     print(msg)
   except KeyboardInterrupt:
--- servidorteste.py
+++ servidorteste.py
@@ -7,10 +7,10 @@ import pickle as p
 sys.path.append('./Classes')
 from descoberta import Descoberta
 
-host = "localhost"
+host = ""
 port = 5000
 
-def config_serve(host="localhost",port=5000):
+def config_serve(host="",port=5000):
   server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
   server.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
   server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
@@ -33,11 +33,9 @@ servidor_thread.start()
 opcoes = "Opções:\n1:Listar Dispositivos conectados\n2:Listar funçoes\n3:Receber dados\n4:descobrir dispositivos\n5:opçoes"
 print("Iniciando Servidor...")
 
-#print("Opções:\n1:Listar Dispositivos conectados\n2:Listar funçoes\n3:Receber dados")
 while True:
   buffer.clear()
-  print("====================================")
-  print(opcoes)
+  print("Op = 5")
   print("====================================")
   comando = input("Digite a opção:")
   try:
@@ -51,18 +49,18 @@ while True:
       print(buffer)
       
     elif comando == '3':
-      msg = ['2',input("Digite o nome do disp:"),input("Digite a função:")]
+      msg = ['2',input("Digite o nome do disp:"),input("Digite o numero da func:"),input("digite o valor:")]
       servidor.sendto(p.dumps(msg), ('<broadcast>', 5680))
       time.sleep(0.5)
       print(buffer)
     
     elif comando == '4':
-      msg = ['1',"nd"]
+      msg = ['1',"ping"]
       servidor.sendto(p.dumps(msg), ('<broadcast>', 5680))
       
     elif comando == '5':
       print(opcoes)
-
+    
   except OSError as msg:
     print(msg)
   except KeyboardInterrupt:
