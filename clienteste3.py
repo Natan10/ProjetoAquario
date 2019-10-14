64c286174619fc3325bf911ad269cd0b9ff3c6dc
RamiroCampos <43554825+RamiroCampos@users.noreply.github.com>
2019-10-14 11:00:46 -0300

    .

--- clienteste3.py
+++ clienteste3.py
@@ -16,7 +16,15 @@ def config_socket(host,port):
 
 
 cliente = config_socket(host,port)
+<<<<<<< Updated upstream
 aquario1 = Portao('Portao1')
+=======
+luz = Luz('Lampada')
+
+#Se idetificando para o servidor
+cliente.sendto(p.dumps(['1','lamp1']),('localhost',5000))
+funcoes = ['1:get_nome','2:get_estado_luz','3:set_estado_luz']
+>>>>>>> Stashed changes
 
 time.sleep(2)
 cliente.sendto(p.dumps('pt1'),('localhost',5000))
@@ -29,7 +37,19 @@ while True:
     data,address = cliente.recvfrom(1024)
     data = p.loads(data)
 
+<<<<<<< Updated upstream
     if data[0] == 'pt1' and data[1] == 'list':
+=======
+    if data[1] == 'lamp1' and data[2] == 'list':
+      print(address) 
+      msg = ['2',funcoes]
+      cliente.sendto(p.dumps(msg),('localhost',5000))
+
+    elif data[0] == '1' and data[1] == 'ping':
+      print(address)
+      msg = ['1','lamp1']
+      cliente.sendto(p.dumps(msg),('localhost',5000))
+>>>>>>> Stashed changes
 
       cliente.sendto(p.dumps(funcoes),('localhost',address))
     elif data[0] == 'pt1' and data[1] in funcoes:
@@ -37,8 +57,22 @@ while True:
       cliente.sendto(p.dumps(msg),('localhost',address)) 
     elif data[0] == '1' and data[1] == 'nd':
       print(address)
+<<<<<<< Updated upstream
       msg = ['1','pt1']
       cliente.sendto(p.dumps(msg),('localhost',5000))
+=======
+      if data[2] == '1':
+        msg = ['2',luz.nome]
+        cliente.sendto(p.dumps(msg),('localhost',5000)) 
+      
+      elif data[2] == '2':
+        msg = ['2',luz.get_estado_luz()]
+        cliente.sendto(p.dumps(msg),('localhost',5000))
+      
+      elif data[2] == '3':
+        msg = ['2',luz.set_estado_luz()]
+        cliente.sendto(p.dumps(msg),('localhost',5000))
+>>>>>>> Stashed changes
 
   except OSError as msg:
     print(msg)
