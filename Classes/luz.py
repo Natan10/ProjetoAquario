b53fa1dee579418d0a150860bc168056acdee6d8
RamiroCampos <43554825+RamiroCampos@users.noreply.github.com>
2019-10-19 21:58:24 -0300

    tempo_ligada da LUZ

--- Classes/luz.py
+++ Classes/luz.py
@@ -8,10 +8,10 @@ import random
 import time 
 
 class Luz:
-  def __init__(self,nome, ligada = 0):
+  def __init__(self,nome, ligada = 0, tempo_ligada = 0):
     self.nome = nome
     self.ligada = ligada
-  
+    self.tempo_ligada = tempo_ligada
   def get_nome(self):
     return self.nome 
 
@@ -24,7 +24,14 @@ class Luz:
   def set_estado_luz(self):
     if self.ligada == 1:
       self.ligada = 0
+      self.tempo_ligada = int(time.time() - self.tempo_ligada)
       return "Luz desligada!" 
     else:
       self.ligada = 1
+      self.tempo_ligada = time.time()
       return "Luz ligada!"
+    
+  def get_tempo_ligada(self):
+      if(self.ligada):
+        return int(time.time() - self.tempo_ligada)
+      return self.tempo_ligada
