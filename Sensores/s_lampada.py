f1b2824c5dbc9a27ad6d63602686e743ae5800a0
RamiroCampos <43554825+RamiroCampos@users.noreply.github.com>
2019-10-19 22:07:21 -0300

    .

---
+++ Sensores/s_lampada.py
@@ -0,0 +1,67 @@
+import sys
+import socket
+import time
+import pickle as p 
+
+sys.path.append('../Funcoes')
+sys.path.append('../Classes')
+from luz import Luz
+from funcoes import *
+
+host = ''
+port = 5680
+
+
+cliente = config_sensor(host,port)
+luz = Luz('Lampada')
+
+#Se idetificando para o servidor
+cliente.sendto(p.dumps(['1','lamp1']),('',5000))
+funcoes = ['1:get_nome','2:get_estado_luz','3:set_estado_luz','4:get_tempo_ligada']
+validacao = ['1','2','3']
+
+print("Iniciando Lampada...")
+
+while True:
+  try: 
+    data,address = cliente.recvfrom(1024)
+    data = p.loads(data)
+
+    if data[1] == 'lamp1' and data[2] == 'list':
+      print(address) 
+      msg = ['2',funcoes]
+      cliente.sendto(p.dumps(msg),('',5000))
+
+    elif data[0] == '1' and data[1] == 'ping':
+      print(address)
+      msg = ['1','lamp1']
+      cliente.sendto(p.dumps(msg),('',5000))
+
+    elif data[1] == 'lamp1':
+      print(address)
+
+      if data[2] not in validacao:
+        msg = ['2','funçao inexistente']
+        cliente.sendto(p.dumps(msg),('',5000))
+      else:
+        if data[2] == '1':
+          msg = ['2',luz.nome]
+          cliente.sendto(p.dumps(msg),('',5000)) 
+        
+        elif data[2] == '2':
+          msg = ['2',luz.get_estado_luz()]
+          cliente.sendto(p.dumps(msg),('',5000))
+        
+        elif data[2] == '3':
+          msg = ['2',luz.set_estado_luz()]
+          cliente.sendto(p.dumps(msg),('',5000))
+          
+        elif data[2] == '4':
+          msg = ['2',luz.get_tempo_ligada()]
+          cliente.sendto(p.dumps(msg),('',5000))
+
+  except OSError as msg:
+    print(msg)
+  except KeyboardInterrupt:
+    print("Encerrando Lampada...")
+    break    
